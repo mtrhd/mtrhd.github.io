73c81f7dab41831beb7b02d9422108530f10a5f2
Łukasz Włódarczyk <lukasz.wlodarczyk@lppsa.com>
2019-06-01 09:40:33 +0200

    gh-page deploy

--- src/App.js
+++ src/App.js
@@ -1,26 +1,21 @@
 import React from 'react';
+import Typed from 'react-typed';
+
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo"/>
+                <p>
+                    <Typed strings={['here you can find anything . . .']} typeSpeed={40}/>
+                </p>
+            </header>
+        </div>
+    );
 }
 
 export default App;
