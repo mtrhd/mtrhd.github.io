f0b35dd9e23e35c49decf5117901aa9d091fd9e8
Łukasz Włódarczyk <lukasz.wlodarczyk@lppsa.com>
2019-06-01 09:38:20 +0200

    Updates

---
+++ precache-manifest.61e002250759cbba308d7bd1dbbfa4e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe89ef9f499f66699b05462abf02f2d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48035d50fe22eb883a2b",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "620a1925d726767866aa",
+    "url": "/static/js/2.bbf2696a.chunk.js"
+  },
+  {
+    "revision": "48035d50fe22eb883a2b",
+    "url": "/static/js/main.9ffcb955.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e45a7e7adfbe050912ba8459dac7bec7",
+    "url": "/static/media/logo.e45a7e7a.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.054774adbe886ee6e3c29227ef1745b5.js"
+  "/precache-manifest.61e002250759cbba308d7bd1dbbfa4e8.js"
 );
 
 self.addEventListener('message', (event) => {
